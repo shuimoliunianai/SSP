25c8235a25aba1a75d9e279dbe2a850958053205
shuimoliunianai <649591475@qq.com>
2016-09-21 18:04:24 +0800

    Update ApplicationTests.java

--- src/test/java/org/gameloft/ssp/ApplicationTests.java
+++ src/test/java/org/gameloft/ssp/ApplicationTests.java
@@ -10,14 +10,13 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * Created by apple on 16/9/20.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = Application.class)
-public class ApplicationTests {
-
+@ContextConfiguration(classes = SspApplicationTestConfig.class)
+public class ApplicationTest {
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
     @Test
     public void test() throws Exception {
-
-        // 保存字符串
-
-
+        stringRedisTemplate.opsForValue().set("aaa", "111");
+        Assert.assertEquals("111", stringRedisTemplate.opsForValue().get("aaa"));
     }
 }
